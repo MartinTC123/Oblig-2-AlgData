1f97f55437c49e2004b0670b192b48b866e147ec
kykauwi <themagnus1208@gmail.com>
2020-10-03 16:07:11 +0200

    Fikset litt i DobbeltLenketListe opprettelsen av noder så alle nodene fikk en .neste node.Små endringer på 2a) før den er ferdig

--- Oblig2/src/no/oslomet/cs/algdat/Main.java
+++ Oblig2/src/no/oslomet/cs/algdat/Main.java
@@ -2,7 +2,7 @@ package no.oslomet.cs.algdat;
 
 public class Main {
     public static void main(String [] args){
-        String [] s = {"A","B"};
+        String [] s = {"A","B","C","D"};
         DobbeltLenketListe<String> liste = new DobbeltLenketListe<>(s);
         System.out.print(liste.toString());
     }
